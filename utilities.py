23e76dce7c9ed57f31f25d6249728a58143162c2
Marius <thartbm@gmail.com>
2024-03-25 14:59:44 -0400

    add function to create data folder structure

--- utilities.py
+++ utilities.py
@@ -1,7 +1,7 @@
 import os, subprocess
 
 def pullGitRepos(repos='all', main=True, clone=False):
-
+    
     if isinstance(repos, str):
         if repos == 'all':
             repos = ['Calibration',
@@ -18,10 +18,13 @@ def pullGitRepos(repos='all', main=True, clone=False):
         synchself=True # do this last: danger zone?
         repos.remove('Runner')
 
+    original_WD = os.getcwd()
+
     os.chdir('..')
     project_dir = os.getcwd()
 
     for repo in repos:
+        print('>> synching: %s'%(repo))
         os.chdir(project_dir)
         # check if the directory exists:
         if os.path.isdir(repo):
@@ -36,33 +39,67 @@ def pullGitRepos(repos='all', main=True, clone=False):
             if clone:
                 subprocess.run(["git", "clone", "https://github.com/Intrepid-2a/%s.git"%(repo)])
 
+    if synchself:
+        print('Not synching myself for now...')
+
+    # get back to original working directory?
+    os.chdir(original_WD)
+
+def setupDataFolders(tasks='all'):
+
+    if isinstance(tasks, str):
+        if tasks == 'all':
+            tasks = ['distance',
+                     'area',
+                     'curvature']
+
+    if not(isinstance(tasks, list)):
+        return
+
+    original_WD = os.getcwd()
+
+    os.chdir('..')
+    project_dir = os.getcwd()
+
+    for task in tasks:
+
+        os.makedirs(os.path.join(project_dir, 'data', task, ''), exist_ok=True)
+
+        for subdir in ['color', 'mapping', 'eyetracking']:
+            os.makedirs(os.path.join(project_dir, 'data', task, subdir, ''), exist_ok=True)
+
+    os.chdir(original_WD)
+
+
+
+
 def collectParticipantInfo():
 
     info = {}
     # how should this be structured?
     # like this?
     empty_participant = {
-        'location'    : ['nowhere'],
+        'location'    : None,
         'distance' :
             {
-                'color'       : [False],
-                'mapping'     : [False],
-                'left'        : [False],
-                'right'       : [False]
+                'color'       : False,
+                'mapping'     : False,
+                'left'        : False,
+                'right'       : False
             },
         'area' :
             {
-                'color'       : [False],
-                'mapping'     : [False],
-                'left'        : [False],
-                'right'       : [False]
+                'color'       : False,
+                'mapping'     : False,
+                'left'        : False,
+                'right'       : False
             },
         'curvature' :
             {
-                'color'       : [False],
-                'mapping'     : [False],
-                'left'        : [False],
-                'right'       : [False]
+                'color'       : False,
+                'mapping'     : False,
+                'left'        : False,
+                'right'       : False
             }
     }
     # then info could be populated with changed versions of the empty participant
