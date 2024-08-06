e18527df171e5ac6954dc3dfe6e96ce1a7691a3b
Marius <thartbm@gmail.com>
2024-08-06 17:03:28 -0400

    use central calibrations for colors and stimuli positions

--- Curvature.py
+++ Curvature.py
@@ -189,12 +189,13 @@ def doCurvatureTask(hemifield=None, ID=None, location=None):
     # unpack all this
     win = setup['win']
 
+    colors = setup['colors']
 
     pyg_keyboard = key.KeyStateHandler()
     win.winHandle.push_handlers(pyg_keyboard)
 
 
-    colors = setup['colors']
+    # colors = setup['colors']
     col_both = colors['both']
     if hemifield == 'left':
         col_ipsi, col_contra = colors['left'], colors['right']
@@ -295,10 +296,24 @@ def doCurvatureTask(hemifield=None, ID=None, location=None):
 
     # shouldn't this be different for left and right hemifield runs?
 
+    bs_prop = setup['blindspotmarkers'][hemifield+'_prop']
+
+            # blindspotmarkers[hemifield+'_prop'] = { 'cart'   : spot_cart,
+            #                                     'spot'   : spot,
+            #                                     'size'   : spot_size,
+            #                                     'tar'    : tar,
+            #                                     'ang_up' : ang_up       }
+
+   
+
     # Padding angles =  BSheight/3 + 2 (dotwidth) + 1(padding) --> value obtained from piloting
-    angpad = spot_righ_size[1]/3 + 2 + 1
+    # angpad = spot_righ_size[1]/3 + 2 + 1
+    angpad = (bs_prop['size'][1]/3) + 2 + 1
     # Padding on circle side
-    side = (spot_righ_size[1] - spot_righ_size[0])*0.15/0.5
+
+    # this could be negative padding?
+    # side = (spot_righ_size[1] - spot_righ_size[0])*0.15/0.5
+    side = (bs_prop['size'][1] - bs_prop['size'][1]) * 0.15/0.5
 
 
     # Protocol paper says:
@@ -316,20 +331,40 @@ def doCurvatureTask(hemifield=None, ID=None, location=None):
     #Positions by hemifield
     if hemifield == 'right':
         # angle division between BS and outside locations = polar angle of the BS x and y + BS size) - angle of the BS location (dev from 0) / 2 + 2(dot stimulus size) + 2 (padding)
-        angup = (cart2pol(spot_righ_cart[0], spot_righ_cart[1] + spot_righ_size[1])[0] - spot_righ[0])/2+ 2 + 2
+        # angup = (cart2pol(spot_righ_cart[0], spot_righ_cart[1] + spot_righ_size[1])[0] - spot_righ[0])/2 + 2 + 2
+
+        angup = (cart2pol(bs_prop['cart'][0], bs_prop['cart'][1] + bs_prop['size'][1])[0] - bs_prop['spot'][0])/2 + 4
 
         # spot_righ[0] is the angle (degrees) of the centre of the blind spot relative to fixation
-        # the cart2pol(...)[0] part returns the angle in of a point 1 blind spot marker higher than the blind spot's centre (in degrees, relative to fixation)
-        # so the difference between those, divided by 2 gives half the height of the blind spot marker in degrees anle, relative to fixation
+        # the cart2pol(...)[0] part returns the angle of a point (relative to fixation) that is:
+        # -  straight above the centre of the blind spot marker
+        # -  but offset by the height of the blind spot marker
+        # so the difference between those, divided by 2
+        # gives something that is usually close to half the height of the blind spot marker in degrees angle, relative to fixation
         # adding 4 degrees angle (not dva!)
 
         #positions
+        # positions = {
+        # "righ-top": [pol2cart(spot_righ[0] + 3*angup, spot_righ[1] - spot_righ_size[0]-side)],
+        # "righ-mid": [pol2cart(spot_righ[0] + angpad,  spot_righ[1] - spot_righ_size[0]-side), 
+        #              pol2cart(spot_righ[0] -angpad, spot_righ[1]- spot_righ_size[0]-side)],
+        # }
+
+
+        # why are we pretending that the blind spot marker is a circle, when we calibrate it as an ellipse?
+        # https://math.stackexchange.com/questions/22064/calculating-a-point-that-lies-on-an-ellipse-given-an-angle
+
         positions = {
-        "righ-top": [pol2cart(spot_righ[0] + 3*angup, spot_righ[1] - spot_righ_size[0]-side)],
-        "righ-mid": [pol2cart(spot_righ[0] + angpad,  spot_righ[1] - spot_righ_size[0]-side), 
-                     pol2cart(spot_righ[0] -angpad, spot_righ[1]- spot_righ_size[0]-side)],
+        "righ-top": [pol2cart(bs_prop['spot'][0] + 3*angup, bs_prop['spot'][1] - bs_prop['size'][0]-side)],
+        "righ-mid": [pol2cart(bs_prop['spot'][0] + angpad,  bs_prop['spot'][1] - bs_prop['size'][0]-side), 
+                     pol2cart(bs_prop['spot'][0] - angpad,  bs_prop['spot'][1] - bs_prop['size'][0]-side)],
         }
+
+
         poss = list(positions.items()) #list of positions used in experiment
+
+        print(poss)
+
         #to make top stimuli parallel to BS
         ydif = (poss[1][1][0][1]-poss[1][1][1][1])/2
         if poss[1][1][0][0] > poss[1][1][1][0]:
@@ -342,12 +377,17 @@ def doCurvatureTask(hemifield=None, ID=None, location=None):
         instructions = visual.TextStim(win, text="Throughout the experiment you will fixate at a a cross located at the centre of the screen. It is important that you maintain fixation on this cross at all times.\n\n In every trial you will be presented with a dot which will move along a curve. You will have to indicate with a keypress if the dot's motion was curved towards fixation or away from fixation  \n \nLeft arrow = motion curved towards fixation.\n \n Right arrow = motion curved away from fixation.\n\n\n You will only be able to respond when the fixation cross rotates from a '+' to a 'x' \n\n\n Press the space bar when you're ready to start the experiment.", color=col_both)
 
     else:
+
+
+        # THIS USES THE RIGHT BLIND SPOT MARKER, NOT THE LEFT!
+
         # angle division between BS and outside locations = polar angle of the BS x and y - BS size) - angle of the BS location (dev from 0) / 2 + 2(dot stimulus size) + 2 (padding)
-        angup = (cart2pol(spot_righ_cart[0], spot_righ_cart[1] - spot_righ_size[1])[0] + spot_righ[0])/2+ 2 + 2
+        angup = (cart2pol(spot_righ_cart[0], spot_righ_cart[1] - spot_righ_size[1])[0] + spot_righ[0])/2 + 2 + 2
+
         # positions
         positions = {
         "left-top": [pol2cart(spot_righ[0] + 3*angup, spot_righ[1] -  spot_righ_size[0]-side)], # this has 1 set of coordinates
-        "left-mid": [pol2cart(spot_righ[0] -angpad,  spot_righ[1] - spot_righ_size[0]-side),    # this has 2 ? are those the aboe & below positions?
+        "left-mid": [pol2cart(spot_righ[0] -angpad,  spot_righ[1] - spot_righ_size[0]-side),    # this has 2 ? are those the above & below positions?
         pol2cart(spot_righ[0] -angpad, spot_righ[1]- spot_righ_size[0]-side)],
         }
         poss = list(positions.items()) #list of positions used in experiment
@@ -374,20 +414,22 @@ def doCurvatureTask(hemifield=None, ID=None, location=None):
     ## Experimental parameters
 
     ## Curvatures, note that 0.000001 instead of 0 to avoid crushing
-    curvature = [0.4, 0.375, 0.35, 0.325, 0.3, 0.275, 0.25,  0.225, 0.2, 0.175, 0.15, 0.125, 0.1, 0.075, 0.05, 0.025, -0.025, 0.000001,-0.000001, -0.05, -0.075, -0.1, -0.125, -0.15, -0.175, -0.2, -0.225, -0.25, -0.275, -0.3, -0.325, -0.35, -0.375, -0.4]
-    curvature = [0.4, 0.375, 0.35, 0.325, 0.3, 0.275, 0.25,  0.225, 0.2, 0.175, 0.15, 0.125, 0.1, 0.075, 0.05, 0.025, 0.000, -0.05, -0.075, -0.1, -0.125, -0.15, -0.175, -0.2, -0.225, -0.25, -0.275, -0.3, -0.325, -0.35, -0.375, -0.4]
+    # curvature = [0.4, 0.375, 0.35, 0.325, 0.3, 0.275, 0.25,  0.225, 0.2, 0.175, 0.15, 0.125, 0.1, 0.075, 0.05, 0.025, -0.025, 0.000001,-0.000001, -0.05, -0.075, -0.1, -0.125, -0.15, -0.175, -0.2, -0.225, -0.25, -0.275, -0.3, -0.325, -0.35, -0.375, -0.4]
+    # curvature = [0.4, 0.375, 0.35, 0.325, 0.3, 0.275, 0.25,  0.225, 0.2, 0.175, 0.15, 0.125, 0.1, 0.075, 0.05, 0.025, 0.000, -0.05, -0.075, -0.1, -0.125, -0.15, -0.175, -0.2, -0.225, -0.25, -0.275, -0.3, -0.325, -0.35, -0.375, -0.4]
 
     # this is 33 values, instead of the 15 we use in the distance task... this should affect the staircases: more trials and reversals needed?
-    curvature = [round((x / 40)-0.4, ndigits=3) for x in list(range(0,33))]
+    # curvature = [round((x / 40)-0.4, ndigits=3) for x in list(range(0,33))]
 
-    # to more or less match the distnace task, we could us this:
-    # curvature = [round((x / 20)-0.4, ndigits=3) for x in list(range(0,17))]
+    curvature = [round((x / 20)-0.4, ndigits=3) for x in list(range(0,17))]
 
     ## staircase
     step = [[[0, 0], [0, 0]], [[0, 0], [0, 0]]] #[['left', 'right'], ['left', 'right']]
     direction = [[[1, -1], [1, -1]], [[1, -1], [1, -1]]] # 2 directions per eye and position converging to straight
     eye = [0, 1] #0 = col_left = red, 1 = col_righ = green
-    eyecol = [col_left, col_righ] 
+    # eye: 0 = ipsi, 1 = contra
+    # eyecol = [col_left, col_righ] # these colors are not defined
+    eyecol = [col_ipsi, col_contra] # use contra and ipsi colors?
+
     revs = [[0, 0], [0, 0]], [[0, 0], [0, 0]] #counter for the number of reversals
     trial = [[0, 0], [0, 0]], [[0, 0], [0, 0]] #counter for the trail number
     Nrevs = 10 #following the dist task - 20/2 - 10
@@ -399,12 +441,13 @@ def doCurvatureTask(hemifield=None, ID=None, location=None):
     choice = []
     #keeping track of time
     trial_clock = core.Clock()
-    blindspot.autoDraw = True
-    #repeated draws 
-    def repeat_draw():
-        fixation.draw()
-        hiFusion.draw()
-        loFusion.draw()
+
+    # blindspot.autoDraw = True # hmmm... turn off when people can look away!
+    # #repeated draws 
+    # def repeat_draw():
+    #     fixation.draw()
+    #     hiFusion.draw()
+    #     loFusion.draw()
 
 
     while not stairs_ongoing == not_ongoing:
